ee5fd2b5a98c0d8ec8fdea49d0036bbc366a6374
yfape <yfape@outlook.com>
2022-05-19 15:15:04 +0800

    '0.0.0'

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <n-layout id="app">
+    <n-layout-header>
+      <n-tabs type="line" animated justify-content="center" size="large">
+        <n-tab name="oasis" tab="Oasis"> Wonderwall </n-tab>
+        <n-tab name="the beatles" tab="the Beatles"> Hey Jude </n-tab>
+        <n-tab name="jay chou" tab="周杰伦"> 七里香 </n-tab>
+      </n-tabs>
+    </n-layout-header>
+    <n-layout has-sider>
+      <n-layout-sider content-style="padding: 24px;"> 海淀桥 </n-layout-sider>
+      <n-layout-content content-style="padding: 24px;">
+        <router-view />
+      </n-layout-content>
+    </n-layout>
+  </n-layout>
 </template>
 
 <style lang="scss">
+html,
+body {
+  width: 100%;
+  height: 100vh;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background-color: #f1f0f5;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.n-tabs-tab-wrapper {
+  padding: 0px 20px;
 }
 </style>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,25 @@
+<template>
+  <n-layout id="app">
+    <n-layout-header>
+      <n-tabs type="line" animated justify-content="center" size="large">
+        <n-tab name="oasis" tab="Oasis"> Wonderwall </n-tab>
+        <n-tab name="the beatles" tab="the Beatles"> Hey Jude </n-tab>
+        <n-tab name="jay chou" tab="周杰伦"> 七里香 </n-tab>
+      </n-tabs>
+    </n-layout-header>
+    <n-layout has-sider>
+      <n-layout-sider content-style="padding: 24px;"> 海淀桥 </n-layout-sider>
+      <n-layout-content content-style="padding: 24px;">
+        <router-view />
+      </n-layout-content>
+    </n-layout>
+  </n-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MainNav",
+});
+</script>
